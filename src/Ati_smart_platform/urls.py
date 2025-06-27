49e33fb3c5568c2a1fea02434cfcfa7b9a753a2a
Ibrahim ATI <ibsaid123@gmail.com>
2025-06-27 17:55:32 +0100

    Dev version

--- src/Ati_smart_platform/urls.py
+++ src/Ati_smart_platform/urls.py
@@ -20,7 +20,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from Ati_smart_platform import views as deployment_views
 
-urlpatterns = [
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
+
+urlpatterns += [
     path('admin/', admin.site.urls),
     path('', include('users.urls', namespace='users')),
     path('run-migrations/', deployment_views.migrate),
